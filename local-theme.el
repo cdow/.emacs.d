98c738bf0beea7d67e22e9085cbc2246e0ec6f0f
Christopher Dow <chris.dow8@gmail.com>
2012-09-21 23:14:41 -0700

    Changed the color of strings in my theme

--- local-theme.el
+++ local-theme.el
@@ -6,7 +6,7 @@
  '(fringe ((t (:background "#969696"))))
  '(mode-line ((t (:foreground "#ffffff" :background "#595959" :box nil))))
  '(font-lock-comment-face ((t (:foreground "#7d827d"))))
- '(font-lock-string-face ((t (:foreground "magenta"))))
+ '(font-lock-string-face ((t (:foreground "#7299ff"))))
  '(font-lock-variable-name-face ((t (:foreground "#e6a00f"))))
  '(minibuffer-prompt ((t (:foreground "#7299ff" :bold t))))
  '(mode-line-highlight ((t (:inverse-video t :box nil)))))
