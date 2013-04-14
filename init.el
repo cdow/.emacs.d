1ec9e10fe1b03f29870cddeea3a993f6edfd1be9
Christopher Dow <chris.dow8@gmail.com>
2013-04-13 21:14:02 -0700

    Added Melpa repo

--- init.el
+++ init.el
@@ -30,9 +30,9 @@
 
 ;;; package
 (require 'package)
-(add-to-list 'package-archives 
-    '("marmalade" .
-      "http://marmalade-repo.org/packages/"))
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+						 ("marmalade" . "http://marmalade-repo.org/packages/")
+						 ("melpa" . "http://melpa.milkbox.net/packages/")))
 (package-initialize)
 
 ;;; evil
